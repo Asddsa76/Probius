b4a9b6f2a62c1da55554cff74637fdfc83b509bc
Asddsa76 <davidlu95@hotmail.com>
2019-10-04 22:11:52 +0200

    Added repo link to help message

--- miscFunctions.py
+++ miscFunctions.py
@@ -9,6 +9,7 @@ def helpMessage():
 	output="[Hero] to see that hero's abilities.\n"
 	output+="[Hero/level] for that hero's talents at that level.\n"
 	output+="[Hero/hotkey] for the ability on that hotkey.\n"
-	output+="[Hero/searchterm] to search for something in that hero's abilities or talents. \n"
-	output+="Emojis: [:Hero/emotion], where emotion is of the following: happy, lol, sad, silly, meh, angry, cool, oops, love, or wow."
+	output+="[Hero/searchterm] to search for something in that hero's abilities or talents.\n"
+	output+="Emojis: [:Hero/emotion], where emotion is of the following: happy, lol, sad, silly, meh, angry, cool, oops, love, or wow.\n"
+	output+="My public repository: https://github.com/Asddsa76/Probius"
 	return output
