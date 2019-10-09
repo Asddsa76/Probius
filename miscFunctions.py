c4804299ba9ea3d6bf9d3211387d9793dfc10d42
Asddsa76 <davidlu95@hotmail.com>
2019-10-09 16:24:18 +0200

    Larger indent

--- miscFunctions.py
+++ miscFunctions.py
@@ -1,12 +1,12 @@
 def extraD(abilities,hero):#Some heroes have weird text in D after < brackets. 
 	if hero =='Samuro':
-		abilities[0]+='\n'+"**Advancing Strikes:** Basic Attacks against enemy Heroes increase Samuro's Movement Speed by 25% for 2 seconds."
+		abilities[0]+='\n'+"    **Advancing Strikes:** Basic Attacks against enemy Heroes increase Samuro's Movement Speed by 25% for 2 seconds."
 	elif hero=='Tassadar':
-		abilities[0]+='\n'+"**Distortion Beam:** Tassadar's Basic Attack is a Distortion Beam that slows enemy units by 25%."
+		abilities[0]+='\n'+"    **Distortion Beam:** Tassadar's Basic Attack is a Distortion Beam that slows enemy units by 25%."
 	elif hero=="Zul'jin":
-		abilities[0]=abilities[0][:-14]+' **You Want Axe?** ***\n ❢ Quest:*** Every 5 Basic Attacks against Heroes permanently increases Basic Attack damage by 1. '
-		abilities[0]+='\n ***? Reward:*** After attacking 75 times, Basic Attack range is increased by 1.1. '
-		abilities[0]+='\n ***? Reward:*** After attacking 150 times, Twin Cleave now revolves twice.'
+		abilities[0]=abilities[0][:-14]+' **You Want Axe?** ***\n    ❢ Quest:*** Every 5 Basic Attacks against Heroes permanently increases Basic Attack damage by 1. '
+		abilities[0]+='\n    ***? Reward:*** After attacking 75 times, Basic Attack range is increased by 1.1. '
+		abilities[0]+='\n    ***? Reward:*** After attacking 150 times, Twin Cleave now revolves twice.'
 	return abilities
 
 def helpMessage():
