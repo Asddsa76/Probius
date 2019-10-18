fc6c3f07780ca2e16353c80306a2d06f7414d608
Asddsa76 <davidlu95@hotmail.com>
2019-10-18 04:54:51 +0200

    Quotes2

---
+++ quotes.py
@@ -0,0 +1,28 @@
+from urllib.request import urlopen
+from aliases import *
+from miscFunctions import getHeroes
+
+def getQuote(hero):
+	with open('quotes.txt','r') as f:
+		for line in f:
+			if hero in line:
+				return line[line.index('; ')+2:]
+
+def downloadQuotes():
+	noQuoteOnPage=['Anduin','Imperius','Mephisto','Murky','Probius','Qhira','The_Butcher','Whitemane']
+	theirQuote=['For the Alliance above all!','I yearn for battle.','*(Hisses)*','Mrrgll','*(Probe sounds)*',"I'm in.",'Fresh meat!','Let the inquisition commence!']
+	with open('quotes.txt','w+') as f:
+		for hero in getHeroes():
+			hero=aliases(hero)
+			print(hero)
+			if hero in noQuoteOnPage:
+				quote=theirQuote[noQuoteOnPage.index(hero)]
+			else:
+				page=''.join([i.strip().decode('utf-8') for i in urlopen('https://heroesofthestorm.gamepedia.com/'+hero)])
+				page=page[page.index('<p><i>"')+7:]
+				quote=page[:page.index('"')]
+			output=hero+'; '+quote
+			f.write(output+'\n')
+
+if __name__=='__main__':
+	downloadQuotes()
