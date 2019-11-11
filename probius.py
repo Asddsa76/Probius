968fb6f1da13738f483a26f26350c36ba89059b8
Asddsa76 <davidlu95@hotmail.com>
2019-11-11 01:13:23 +0100

    Refactored logging message

--- probius.py
+++ probius.py
@@ -24,8 +24,9 @@ from quotes import *			#Lock-in quotes
 from stats import stats
 
 async def mainProbius(client,message,texts):
-	print(message.channel.guild.name+' '*(15-len(message.channel.guild.name))+message.channel.name+' '+' '*(17-len(message.channel.name))+str(message.author)+' '*(18-len(str(message.author)))+' '+message.content)
-	await client.get_channel(643231901452337192).send('`'+message.channel.guild.name+' '*(15-len(message.channel.guild.name))+message.channel.name+' '+' '*(17-len(message.channel.name))+str(message.author)+' '*(18-len(str(message.author)))+' '+message.content+'`')
+	loggingMessage=message.channel.guild.name+' '*(15-len(message.channel.guild.name))+message.channel.name+' '+' '*(17-len(message.channel.name))+str(message.author)+' '*(18-len(str(message.author)))+' '+message.content
+	print(loggingMessage)
+	await client.get_channel(643231901452337192).send('`'+loggingMessage+'`')
 	for text in texts:
 		hero=text[0]
 		buildsAliases=['guide','build','b','g','builds','guides']
