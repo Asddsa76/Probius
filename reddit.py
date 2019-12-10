1ad066b98498a47bdc6892c9abdc71eb109aedf7
Asddsa76 <davidlu95@hotmail.com>
2019-12-10 02:57:09 +0100

    Sam forwarding

--- reddit.py
+++ reddit.py
@@ -8,6 +8,8 @@ redditors=['Asddsa76', 'Blackstar_9', 'Spazzo965', 'SomeoneNew666', 'joshguillen
 discordnames={'Pscythic':'Soren Ily', 'SotheBee':'Sothe', 'slapperoni':'slap','secret3332':'SecretChaos','Archlichofthestorm':'Trolldareon','ThatDoomedStudent':'Carbon','InfiniteEarth':'Flash',
 'KillMeWithMemes':'Nick','ridleyfire':'HailFall','bran76765':'Parthuin','Demon_Ryu':'Messa'}
 
+keywords=['genji','samuro']#Posts with these in title gets forwarded regardless of author
+
 async def getPostInfo(post):
 	title=post.split('", "')[0]
 	post=post.split('"author": "')[1]
@@ -30,34 +32,36 @@ async def fillPreviousPostTitles(client):#Called on startup
 		for post in posts:
 			[title,author,url] = await getPostInfo(post)#Newest post that has been checked
 			output.append(title)
-			if author in redditors or 'genji' in title.lower():
+			if author in redditors or sum(1 for i in keywords if i in title.lower()):
 				title=title.replace('&amp;','&').replace('\u2013','-').replace('\u0336','')
 				client.forwardedPosts.append([title,author,url])
 		client.forwardedPosts=client.forwardedPosts[::-1]
 		return output
 
 async def redditForwarding(client):#Called every 60 seconds
-	#try:
-	async with aiohttp.ClientSession() as session:
-		page = await fetch(session, 'https://old.reddit.com/r/heroesofthestorm/new.api')#Screw JSON parsing, I'll do it myclient
-		posts=page.split('"clicked": false, "title": "')[1:]
-		for post in posts:
-			[title,author,url] = await getPostInfo(post)
-			if author in redditors or 'genji' in title.lower():
-				if title not in client.seenTitles:#This post hasn't been processed before
-					client.seenTitles.append(title)
-					title=title.replace('&amp;','&').replace('\u2013','-').replace('\u0336','')
-					client.forwardedPosts.append([title,author,url])
-					if 'genji' in title.lower():
-						await client.get_channel(568058278165348362).send('**'+title+'** <@183240974347141120><@247677408386351105>'+url)#Normie heroes
-					else:
-						if author in discordnames:
-							author=discordnames[author]
-						await client.get_channel(557366982471581718).send('**'+title+'** by '+author+': '+url)#General
-					await client.get_channel(643231901452337192).send('`'+title+' by '+author+'`')#log
-					print(title+' by '+author)
-					if author=='Gnueless':
-						await rotation(channel)
-	'''except:
+	try:
+		async with aiohttp.ClientSession() as session:
+			page = await fetch(session, 'https://old.reddit.com/r/heroesofthestorm/new.api')#Screw JSON parsing, I'll do it myclient
+			posts=page.split('"clicked": false, "title": "')[1:]
+			for post in posts:
+				[title,author,url] = await getPostInfo(post)
+				if author in redditors or sum(1 for i in keywords if i in title.lower()):
+					if title not in client.seenTitles:#This post hasn't been processed before
+						client.seenTitles.append(title)
+						title=title.replace('&amp;','&').replace('\u2013','-').replace('\u0336','')
+						client.forwardedPosts.append([title,author,url])
+						if 'genji' in title.lower():
+							await client.get_channel(568058278165348362).send('**'+title+'** <@183240974347141120><@247677408386351105>'+url)#Normie heroes
+						elif 'samuro' in title.lower():
+							await client.get_channel(564528564196605973).send('**'+title+'** <@329447886465138689>'+url)#Samuro-general
+						else:
+							if author in discordnames:
+								author=discordnames[author]
+							await client.get_channel(557366982471581718).send('**'+title+'** by '+author+': '+url)#General
+						await client.get_channel(643231901452337192).send('`'+title+' by '+author+'`')#log
+						print(title+' by '+author)
+						if author=='Gnueless':
+							await rotation(channel)
+	except:
 		await client.get_channel(643231901452337192).send('Something went wrong with subreddit forwarding')
-		print('Something went wrong with subreddit forwarding')'''
+		print('Something went wrong with subreddit forwarding')
