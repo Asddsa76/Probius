47543511b78a6dbf35b23c6cd8b981811fd50c8d
Asddsa76 <davidlu95@hotmail.com>
2022-03-17 19:01:16 +0100

    Wandering Vagrant + sorting fix

--- sorting.py
+++ sorting.py
@@ -2,7 +2,7 @@ from discordIDs import *
 from lfg import roleAliases
 
 async def trim(text):
-	toRemove=[' ','#','<@&{}>'.format(DiscordRoleIDs['Olympian']),'*','\n','league',' and']
+	toRemove=[' ','#','<@&{}>'.format(DiscordRoleIDs['Olympian']),'*','\n','league','and']
 	text=text.lower()
 	for i in toRemove:
 		text=text.replace(i,'')
